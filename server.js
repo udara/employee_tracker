90e002b22bddedc515e1e16bf61ac0e723b8332a
Udara ranasinghe <indi79@hotmail.com>
2020-05-20 11:48:47 +0930

    addming comments

--- server.js
+++ server.js
@@ -19,16 +19,16 @@ const helper = new Helper();
 
 async function init()
 {
-  while (true) {
+  while (true) { // Loop till user picks option that is not in the menu. (i.e Exit Program)
 
     const { main_menu_choice }  = await helper.promptMainMenu();
     
     switch (main_menu_choice) {
       case 'Add Departments':
         console.clear();
-        console.log(colors.bgBlue('Add Departments'));
+        console.log( colors.bgBlue('Add Departments') );
         const {department_name} = await helper.promptAddDepartment();
-        await db.insertDepartment(department_name);
+        await db.insertDepartment( department_name ); 
         break;
 
       case 'Add Employee Roles':
@@ -40,9 +40,9 @@ async function init()
 
       case 'Add Employees':
         console.clear();
-        console.log(colors.bgBlue('Add Employees'));
+        console.log( colors.bgBlue('Add Employees') );
         const employee_data= await helper.promptAddEmployee( await db.getEmployeeRole() , await db.getManager());
-        await db.insertEmployee(employee_data);
+        await db.insertEmployee( employee_data );
         break;
       
       case 'View Departments':
@@ -53,34 +53,34 @@ async function init()
 
       case 'View Roles':
         console.clear();
-        console.log(colors.bgBlue('View Roles'));
+        console.log( colors.bgBlue('View Roles') );
         console.table( await db.displayEmployeeRole() );   
         break;
 
       case 'View Employees':
         console.clear();
-        console.log(colors.bgBlue('View Employees'));
+        console.log( colors.bgBlue('View Employees') );
         console.table( await db.displayEmployee() );
         break;
 
       case 'View Employees by Manager':
         console.clear();
-        console.log(colors.bgBlue('View Employees by Manager'));
+        console.log( colors.bgBlue('View Employees by Manager') );
         const {manager_id} = await helper.promptgetManager( await db.getManager() );
-        console.table( await db.displayEmployeebyManager(manager_id) );
+        console.table( await db.displayEmployeebyManager( manager_id ) );
         break;
 
       case 'Update Employee Roles':
         console.clear();
         console.log(colors.bgBlue('Update Employee Roles'));
-        const { employee_id_role_update, new_role_id } = await helper.promptUpdateEmployeeRole(await db.getEmployee() , await db.getEmployeeRole())
-        db.updateEmployeeRole(employee_id_role_update, new_role_id);
+        const { employee_id_role_update, new_role_id } = await helper.promptUpdateEmployeeRole( await db.getEmployee() , await db.getEmployeeRole() )
+        db.updateEmployeeRole( employee_id_role_update , new_role_id );
       break;
 
       case 'Update Employee Manager':
         console.clear();
         console.log(colors.bgBlue('Update Employee Manager'));
-        const { employee_id, new_manager_id } = await helper.promptUpdateManager( await db.getEmployee() );
+        const { employee_id, new_manager_id } = await helper.promptUpdateManager( await db.getEmployee(), await db.getManager()  );
         db.updateEmployeeManager( employee_id, new_manager_id );
         break;
 
@@ -102,14 +102,14 @@ async function init()
         console.clear();
         console.log(colors.bgBlue('Delete Employee'));  
         const { employee_id_to_delete } =  await helper.promptDeleteEmpoyee( await db.getEmployee() );
-        db.promptDeleteEmpoyee(employee_id_to_delete);
+        db.deleteEmployee( employee_id_to_delete );
         break;
 
         case 'Calc Department Salaries':
           console.clear();
           console.log(colors.bgBlue('Calc Department Salaries'));
           const { department_id_to_calc_budget } =  await helper.promptCalculateDepartmentSum( await db.getDepartments() );
-          console.table( await db.budgetForSelectedDepartment(department_id_to_calc_budget)); 
+          console.table( await db.budgetForSelectedDepartment( department_id_to_calc_budget )); 
           break;
     
       default:
